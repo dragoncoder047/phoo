1f5e182a15670153c405c30be36561c3c465683d
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-22 08:40:49 -0400

    test imports

--- app/main.js
+++ app/main.js
@@ -1,13 +1,25 @@
+import { Phoo, initBuiltins } from '../src/index.js';
+
 function $(sel) { return document.querySelector(sel); }
 
 var term = new Terminal();
 term.open($('main'));
 term.write('Phoo is loading... ')
-var x = '/-|\\';
-var i = 0;
-(function test() {
-    term.write('\b');
-    term.write(x[i]);
-    if (++i == x.length) i = 0;
-    setTimeout(test, 50);
+
+var loading = (function load() {
+    var x = '/-|\\';
+    var i = 0;
+    (function test() {
+        term.write('\b');
+        term.write(x[i]);
+        if (++i == x.length) i = 0;
+        setTimeout(test, 250);
+    })();
 })();
+
+// do load
+
+return;
+
+clearTimeout(loading);
+term.clear();
