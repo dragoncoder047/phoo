652b67439fd7fb073ea452b7e37c26258141d714
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-22 11:03:06 -0400

    test more using code from xterm demo

--- app/main.js
+++ app/main.js
@@ -2,8 +2,10 @@ import { Phoo, initBuiltins } from '../src/index.js';
 
 function $(sel) { return document.querySelector(sel); }
 
-var term = new Terminal();
-term.open($('main'));
+var term = new Terminal({
+    cursorBlink: true,
+});
+term.open($('#terminal'));
 term.write('Phoo is loading... ')
 
 var loading = (function load() {
@@ -23,15 +25,52 @@ clearTimeout(loading);
 term.clear();
 term.writeln('Hello world')
 
-var STDIN = '';
+const PROMPT_1 = '\x1b[31m->\x1b[0m '
+const PROMPT_2 = '\x1b[31m..\x1b[0m ';
+const PROMPT_WIDTH = 3;
+var command = '';
+var multiline = false;
 
 term.onData(data => {
-    STDIN += data;
-    term.writeln(data);
-    if (data == '\n') {
-        term.write(`\x1b[31m${STDIN}\x1b[0m`);
-        STDIN = '';
+    switch (data) {
+        case '\x03': // Ctrl+C
+            term.write('^C');
+            kill();
+            term.write('\r\n' + PROMPT_1);
+            break;
+        case '\r': // Enter
+            [multiline, command] = runCommand(command);
+            term.write(multiline ? PROMPT_2 : PROMPT_1);
+            break;
+        case '\x7F': // Backspace (DEL)
+            // Do not delete the prompt
+            if (term._core.buffer.x > PROMPT_WIDTH) {
+                term.write('\b \b'); // back up and erase a char
+                if (command.length > 0) {
+                    command = command.substring(0, command.length - 1);
+                }
+                else {
+                    term.write('\a'); // bell
+                }
+            }
+            else {
+                term.write('\a'); // bell
+            }
+            break;
+        default: // Print all other characters
+            if (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7B) || e >= '\u00a0') {
+                command += e;
+                term.write(e);
+            }
     }
 });
+
+function runCommand(c) {
+    term.writeln(`\x1b[31m${c}\x1b[0m`);
+}
+
+function kill() {
+    term.writeln('\x1b[31mKilled\x1b[0m')
+}
 
 term.focus();
