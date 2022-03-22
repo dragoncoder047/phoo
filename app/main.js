8f83ef49082b94247be4b86269723f32df2a417c
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-22 13:20:29 -0400

    test using boilerplate from xtermjs.org

--- app/main.js
+++ app/main.js
@@ -21,57 +21,56 @@ var loading = (function load() {
 
 // do load
 
-return;
-clearTimeout(loading);
-term.clear();
-term.writeln('Hello world')
+// clearTimeout(loading);
+// term.clear();
+// term.writeln('Hello world')
 
-const PROMPT_1 = '\x1b[31m->\x1b[0m '
-const PROMPT_2 = '\x1b[31m..\x1b[0m ';
-const PROMPT_WIDTH = 3;
-var command = '';
-var multiline = false;
+// const PROMPT_1 = '\x1b[31m->\x1b[0m '
+// const PROMPT_2 = '\x1b[31m..\x1b[0m ';
+// const PROMPT_WIDTH = 3;
+// var command = '';
+// var multiline = false;
 
-term.onData(data => {
-    switch (data) {
-        case '\x03': // Ctrl+C
-            term.write('^C');
-            kill();
-            term.write('\r\n' + PROMPT_1);
-            break;
-        case '\r': // Enter
-            [multiline, command] = runCommand(command);
-            term.write(multiline ? PROMPT_2 : PROMPT_1);
-            break;
-        case '\x7F': // Backspace (DEL)
-            // Do not delete the prompt
-            if (term._core.buffer.x > PROMPT_WIDTH) {
-                term.write('\b \b'); // back up and erase a char
-                if (command.length > 0) {
-                    command = command.substring(0, command.length - 1);
-                }
-                else {
-                    term.write('\a'); // bell
-                }
-            }
-            else {
-                term.write('\a'); // bell
-            }
-            break;
-        default: // Print all other characters
-            if (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7B) || e >= '\u00a0') {
-                command += e;
-                term.write(e);
-            }
-    }
-});
+// term.onData(data => {
+//     switch (data) {
+//         case '\x03': // Ctrl+C
+//             term.write('^C');
+//             kill();
+//             term.write('\r\n' + PROMPT_1);
+//             break;
+//         case '\r': // Enter
+//             [multiline, command] = runCommand(command);
+//             term.write(multiline ? PROMPT_2 : PROMPT_1);
+//             break;
+//         case '\x7F': // Backspace (DEL)
+//             // Do not delete the prompt
+//             if (term._core.buffer.x > PROMPT_WIDTH) {
+//                 term.write('\b \b'); // back up and erase a char
+//                 if (command.length > 0) {
+//                     command = command.substring(0, command.length - 1);
+//                 }
+//                 else {
+//                     term.write('\a'); // bell
+//                 }
+//             }
+//             else {
+//                 term.write('\a'); // bell
+//             }
+//             break;
+//         default: // Print all other characters
+//             if (e >= String.fromCharCode(0x20) && e <= String.fromCharCode(0x7B) || e >= '\u00a0') {
+//                 command += e;
+//                 term.write(e);
+//             }
+//     }
+// });
 
-function runCommand(c) {
-    term.writeln(`\x1b[31m${c}\x1b[0m`);
-}
+// function runCommand(c) {
+//     term.writeln(`\x1b[31m${c}\x1b[0m`);
+// }
 
-function kill() {
-    term.writeln('\x1b[31mKilled\x1b[0m')
-}
+// function kill() {
+//     term.writeln('\x1b[31mKilled\x1b[0m')
+// }
 
-term.focus();
+// term.focus();
