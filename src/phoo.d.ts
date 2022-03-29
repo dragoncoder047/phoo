4b279617d6734a63fbb8ae754c6c36ec718c84df
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-29 14:36:44 -0400

    update .d.ts

--- src/phoo.d.ts
+++ src/phoo.d.ts
@@ -2,7 +2,7 @@ declare module 'phoo' {
     interface IPhooSettings {
         maxDepth?: number,
         strictMode?: boolean,
-        namepathSeparator?: string
+        namepathSeparator?: string,
     }
 
     interface IPhooOptions {
@@ -18,6 +18,49 @@ declare module 'phoo' {
         createThread(module: string, scopes: Scope[], modules: Module[], starModules: Module[], stack: any[]): Thread;
         findModule(moduleName: string): Module;
     }
+}
 
-    declare type IPhooRunnable = Function | Array<IPhooRunnable>;
+declare module 'phoo/threading' {
+    interface IThreadOptions {
+        parent: Phoo,
+        module: Module,
+        starModules: Module[],
+        modules: Map<string, Module>,
+        stack: any[],
+        scopes: Scope[],
+        maxDepth: number,
+    }
+
+    export declare class Thread {
+        phoo: Phoo;
+        module: Module;
+        starModules: Module[];
+        workStack: any[];
+        scopeStack: Scope[];
+        returnStack: IReturnStackEntry[];
+        maxDepth: number;
+        lock: Threadlock;
+        constructor(opts: IThreadOptions): Thread;
+        async executeOneItem(item: IPhooRunnable): void;
+        async compileLiteral(word: string, a: IPhooRunnable[]): boolean;
+        async compile(source: string | any[] | IPhooRunnable[], hasLockAlready?: boolean): IPhooRunnable[];
+        expect(...types: (string | RegExp | number)[]): void;
+        pop(depth: number): any;
+        peek(depth: number): any;
+        push(item: any): void;
+        retPop(): IReturnStackEntry;
+        retPush(item: IReturnStackEntry): void;
+        enterScope(): void;
+        exitScope(): void;
+        async execute(c: IPhooRunnable, hasLockAlready?: boolean): any[];
+        async run(source: string | any[] | IPhooRunnable[], hasLockAlready?: boolean): any[];
+        getScope(idx: number): Scope;
+        resolveNamepath(word: string, where: 'words' | 'macros'): IPhooDefinition;
+
+    }
+
+    declare type IPhooRunnable = Function | IPhooLiteral | IPhooRunnable[];
+    declare type IPhooLiteral = any;
+    declare type IReturnStackEntry = { pc: number; arr: IPhooRunnable[] };
+    declare type IPhooDefinition = Function | Symbol | IPhooRunnable;
 }
