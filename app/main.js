82f8036630ca5172523d0b860e7900c007090931
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-05-17 12:59:43 -0400

    add jQuery colorize?

--- app/main.js
+++ app/main.js
@@ -5,7 +5,7 @@ var count = 0;
 var run;
 const esc = $.terminal.escape_brackets;
 const naiveColorize = (text, color) => `[[;${color};]${esc(text)}]`;
-const color = (text, color) => naiveColorize(esc(text), color);
+const color = (text, color) => $('<span>').text(text).css('color', color);
 var p, thread;
 
 const term = $('body').terminal(c => run(c), {
@@ -65,7 +65,7 @@ var loading = true;
                 term.error('Error! ' + type(e) !== 'string' ? e.message : e);
                 term.error(e[STACK_TRACE_SYMBOL] || 'No stack trace');
             }
-            term.echo('Stack: ' + stringify(thread.workStack, naiveColorize));
+            term.echo('Stack: ' + stringify(thread.workStack, color));
             count++;
         };
 
@@ -81,7 +81,7 @@ var loading = true;
         term.error('Phoo stack trace:');
         term.error(e[STACK_TRACE_SYMBOL]);
         term.echo('Thread work stack:');
-        term.echo(stringify(thread.workStack));
+        term.echo(stringify(thread.workStack, color));
         term.echo('If this continues to occur, please report it:');
         term.echo('https://github.com/dragoncoder047/phoo/issues');
         term.disable();
